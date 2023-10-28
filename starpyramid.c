c40c9f1846bc753c42e8e870eaac8d972b2be384
Doney Siby <145443681+doney25@users.noreply.github.com>
2023-10-28 20:15:55 +0530

    Star Pyramid

---
+++ starpyramid.c
@@ -0,0 +1,22 @@
+
+    #include<stdio.h>
+    void main()
+    {
+        int rows;
+        printf("Enter the number of rows : ");
+        scanf(" %d", &rows);
+
+        for(int i=0; i<rows; i++)
+        {
+            for(int j=0;j<rows-1-i;j++)
+            {
+                printf(" ");
+            }
+            for(int k=0; k<;k++)
+            {
+            	  printf("* ");
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
